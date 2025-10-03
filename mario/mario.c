e2f70afc758d175e5a08e063e56813cf374c55d0
sabr.sf <91415147+pythonhack000@users.noreply.github.com>
2025-10-03 07:10:11 +0300

    Update mario.c
    Signed-off-by: sabr.sf <91415147+pythonhack000@users.noreply.github.com>

--- mario/mario.c
+++ mario/mario.c
@@ -1,6 +1,15 @@
 #include <cs50.h>
 #include <stdio.h>
 
+// for output of #
+void print_hashes(int count)
+{
+    for (int i = 1; i <= count; i++)
+    {
+        printf("#");
+    }
+}
+
 int main(void)
 {
     int height;
@@ -11,24 +20,15 @@ int main(void)
     }
     while (height < 1 || height > 8);
 
-    for (int i = 0; i < height; i++)
+    for (int i = 1; i <= height; i++)
     {
-        for (int j = 0; j < height - i - 1; j++)
+        for (int j = 1; j <= height - i; j++)
         {
             printf(" ");
         }
-
-        for (int j = 0; j <= i; j++)
-        {
-            printf("#");
-        }
-
+        print_hashes(i);
         printf("  ");
-
-        for (int j = 0; j <= i; j++)
-        {
-            printf("#");
-        }
+        print_hashes(i);
 
         printf("\n");
     }
